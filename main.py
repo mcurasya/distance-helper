5b0ca9a38ac082e0d1971920501155c5316fd219
yura <mcurasya@gmail.com>
2020-04-20 19:21:44 +0300

    word to doc

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import config
 from PIL import Image
 import os
 import shutil
-import comtypes.client
-
+import convertapi
 bot = telebot.TeleBot(config.TOKEN)
 savepdfconst = 17
 
@@ -79,22 +78,22 @@ def handle_rename(message):
 
 @bot.message_handler(content_types=['document'])
 def handle_word_to_pdf(message):
-    word = comtypes.client.CreateObject('Word.Application')
-    word.Visible = False
+    print("handle word to doc")
     doc_info = bot.get_file(message.document.file_id)
     doc_file = bot.download_file(doc_info.file_id)
     with open(f"temp_{message.from_user.id}.doc", "wb") as writable:
         writable.write(doc_file)
-    
-    doc = word.Documents.open('temp_{message.from_user.id}.doc')
-    doc.SaveAs(f"temp_{message.from_user.id}.pdf", FileFormat=savepdfconst)
-    doc.Close()
-    word.Quit()
-
-    with open(f"temp_{message.from_user.id}.pdf", "rb") as f:
-        bot.send_document(message.chat.id, f)
-    os.remove(f"temp_{message.from_user.id}.pdf")
+    print("got doc")
+    convertapi.api_secret = config.SECRET
+    convertapi.convert('pdf', {
+        "File": f"temp_{message.from_user.id}.doc"
+    }, from_format='doc').save_files(f"temp_{message.from_user.id}.pdf")
+    print("got pdf")
     os.remove(f"temp_{message.from_user.id}.doc")
+    with open(f"temp_{message.from_user.id}.pdf", "rb") as pdf:
+        bot.send_document(message.from_user.id, pdf)
+    os.remove(f"temp_{message.from_user.id}.pdf")
+    print("pdf sent")
 
 
 print("start polling")
